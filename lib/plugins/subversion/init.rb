9ae008b2a2221581a434e310ea4434b198c16008
Niklas Hofer <niklas+dev@lanpartei.de>
2008-08-29 23:13:12 +0200

    overlay file icons with proper subversion icons from http://tortoisesvn.net/node/138

---
+++ lib/plugins/subversion/init.rb
@@ -0,0 +1,5 @@
+#Requirer.require_if('vimmatelib/subversion') do
+  require 'vimmatelib/subversion'
+  require File.dirname(__FILE__) + '/lib/file'
+  VimMate::ListedFile.class_eval { include VimMate::Plugin::SubversionFile }
+#end
