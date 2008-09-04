eb56947d97a8bd2cd939fd3e81c74c66977ca712
Niklas Hofer <niklas+dev@lanpartei.de>
2008-09-04 18:47:15 +0200

    add vimmatelib and lib to load paths, svn plugin loads only if svn is avaiable

--- lib/plugins/subversion/init.rb
+++ lib/plugins/subversion/init.rb
@@ -1,6 +1,5 @@
-#Requirer.require_if('vimmatelib/subversion') do
-  require 'vimmatelib/subversion'
+VimMate::Requirer.require_if('subversion') do
   require File.dirname(__FILE__) + '/lib/file'
   VimMate::ListedFile.class_eval { include VimMate::Plugin::SubversionFile }
   VimMate::ListedDirectory.class_eval { include VimMate::Plugin::SubversionFile }
-#end
+end
--- lib/vimmatelib/plugins.rb
+++ lib/vimmatelib/plugins.rb
@@ -1,3 +1,4 @@
+require 'requirer'
 plugin_dir = File.expand_path File.dirname(__FILE__) + '/../plugins'
 Dir["#{plugin_dir}/*/init.rb"].each do |plugin_init|
   require plugin_init
