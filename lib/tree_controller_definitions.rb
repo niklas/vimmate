6b2c6c47ceaa3dc3e67bfb7410595502fd1fcfdc
Niklas Hofer <niklas+dev@lanpartei.de>
2008-09-04 19:11:59 +0200

    Tree Definitions can now be included properly

--- lib/tree_controller_definitions.rb
+++ lib/tree_controller_definitions.rb
@@ -2,25 +2,34 @@ module VimMate
   module Tree
     module Definitions
       module Column
-        @@columns = []
-        def self.column(label,type)
-          @@columns << [label.to_sym, type]
-          const_set label.to_s.upcase, @@columns.length-1
+        def self.included(base)
+          base.class_eval do
+            include InstanceMethods
+            extend ClassMethods
+          end
         end
-        def self.columns
-          @@columns
+        module InstanceMethods
+          def columns
+            self.class.columns
+          end
+          def columns_labels
+            self.class.columns_labels
+          end
         end
-        def columns
-          self.class.columns
-        end
-        def self.columns_labels
-          columns.collect { |c| c[0] }
-        end
-        def columns_labels
-          self.class.columns_labels
-        end
-        def self.columns_types
-          columns.collect { |c| c[1] }
+        module ClassMethods
+          def column(label,type)
+            columns << [label.to_sym, type]
+            const_set label.to_s.upcase, @@columns.length-1
+          end
+          def columns
+            @@columns ||= []
+          end
+          def columns_labels
+            columns.collect { |c| c[0] }
+          end
+          def columns_types
+            columns.collect { |c| c[1] }
+          end
         end
       end
     end
