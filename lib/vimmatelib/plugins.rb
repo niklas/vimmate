16242ff4d07e77761bcb27f85f452051a788750e
Niklas Hofer <niklas+dev@lanpartei.de>
2008-09-06 22:05:38 +0200

    add plugin path to load path

--- lib/vimmatelib/plugins.rb
+++ lib/vimmatelib/plugins.rb
@@ -1,5 +1,6 @@
 require 'requirer'
 plugin_dir = File.expand_path File.dirname(__FILE__) + '/../plugins'
 Dir["#{plugin_dir}/*/init.rb"].each do |plugin_init|
+  $:.unshift File.dirname(plugin_init)
   require plugin_init
 end
