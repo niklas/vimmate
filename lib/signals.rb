4795e455f354902a0eba0853c5d3563970c9d3fd
Niklas Hofer <niklas+dev@lanpartei.de>
2008-09-08 22:05:26 +0200

    Signal.define create on_ and emit_ methods

--- lib/signals.rb
+++ lib/signals.rb
@@ -1,21 +1,47 @@
 module VimMate
-  class Signals
-    #include Singleton
-
+  class UnknownSignalError < StandardError ; end
+  class Signal
     @@signals = {}
 
-    def self.connect(signal, &block)
-      @@signals[signal] ||= []
-      @@signals[signal] << block
+    def self.on(name, &block)
+      @@signals[name] ||= []
+      @@signals[name] << block
     end
 
-    def self.emit(signal, *args)
-      if @@signals[signal].is_a?(Array)
-        @@signals[signal].each do |block|
-          block.call(args)
+    def self.emit(name, *args)
+      if @@signals[name].is_a?(Array)
+        @@signals[name].each do |block|
+          block.call(*args)
         end
       end
     end
+
+    def self.define name
+      class_eval <<-EOCODE
+        def self.emit_#{name}(*args)
+          self.emit(:#{name},*args)
+        end
+        def self.on_#{name}(&block)
+          self.on(:#{name}, &block)
+        end
+      EOCODE
+    end
+
+    def self.method_missing(meth,*args,&block)
+      case meth.to_s
+      when /^emit_(.*)$/
+        raise UnknownSignalError, "cannot emit unknown signal '#{$1}'"
+      when /^on_(.*)$/
+        raise UnknownSignalError, "cannot connect to unknown signal '#{$1}'"
+      else
+        raise NoMethodError, "unknown method: #{meth}"
+      end
+    end
+    
+    define :file_created
+    define :file_modified
+    define :file_deleted
+    define :file_opened
 
   end
 end
