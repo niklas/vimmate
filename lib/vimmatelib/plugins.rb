eb02d2c4ea36e59a0155bdbd0efe04e574dabd0f
Niklas Hofer <niklas+dev@lanpartei.de>
2009-05-21 19:18:55 +0200

    forgot to notify git of the rm part of movement

--- lib/vimmatelib/plugins.rb
+++
@@ -1,6 +0,0 @@
-require 'requirer'
-plugin_dir = File.expand_path File.dirname(__FILE__) + '/../plugins'
-Dir["#{plugin_dir}/*/init.rb"].each do |plugin_init|
-  $:.unshift File.dirname(plugin_init)
-  require plugin_init
-end
