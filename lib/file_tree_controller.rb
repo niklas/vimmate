70c601851e9ae91b064b5af9c04d8f791d555c67
Niklas Hofer <niklas+dev@lanpartei.de>
2009-05-22 01:47:52 +0200

    cache if the filter string has a slash

--- lib/file_tree_controller.rb
+++ lib/file_tree_controller.rb
@@ -167,11 +167,20 @@
 
     private
     def iter_visible_through_filter?(iter)
-      if filter_string.index('/')
+      if filter_with_slash?
         iter[ListedFile.full_path_column]
       else
         iter[ListedFile.name_column]
       end =~ filter_regexp
+    end
+
+    def filter_with_slash?
+      @filter_with_slash = filter_string.index('') if @filter_with_slash.nil?
+      @filter_with_slash
+    end
+    after_filter_applied :reset_filter_with_slash
+    def reset_filter_with_slash
+      @filter_with_slash = nil
     end
 
     # Fuzzy search by String
