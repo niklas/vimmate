19b94c4fe3fb54273b9bb531e82270f14326de5d
Niklas Hofer <niklas+dev@lanpartei.de>
2009-05-24 19:23:51 +0200

    root path is shown again!

--- lib/file_tree_store.rb
+++ lib/file_tree_store.rb
@@ -1,11 +1,11 @@
 class FileTreeStore < ActiveWindow::ActiveTreeStore
-  column :full_path, String
   column :icon, Gdk::Pixbuf
+  column :full_path, String, :virtual => true
+  column :name, String
   column :status, String
 
-  def add_path(path)
-    STDERR.puts "ading #{path}"
+  def add_path(path, parent=nil)
+    add :full_path => File.expand_path(path), :name => File.basename(path), :status => 'normal'
   end
-
 
 end
