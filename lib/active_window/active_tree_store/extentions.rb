b5e52a0024182e56812f0f51bfce8d77dc00dcfa
Niklas Hofer <niklas+dev@lanpartei.de>
2009-05-24 19:24:52 +0200

    can add data to store form object or hash

--- lib/active_window/active_tree_store/extentions.rb
+++ lib/active_window/active_tree_store/extentions.rb
@@ -13,8 +13,8 @@ module ActiveWindow
       cols = used_columns
       cols = cols.except(opts[:except]) if opts.has_key?(:except)
       cols = cols.slice(opts[:only]) if opts.has_key?(:only)
-      cols.each do |column|
-        treeview.append_column(column.view)
+      cols.map(&:view).each do |column|
+        treeview.append_column(column)
       end
     end
 
@@ -34,8 +34,11 @@ module ActiveWindow
     # Add +object+ to tree, give optional +parent+
     def add(object, parent=nil)
       iter = self.append parent
-      used_columns.each do |column|
-        iter[column.id] = column.data_value(object)
+      case object
+      when Hash
+        update_iter_from_hash iter, object
+      else
+        update_iter_from_object iter, object
       end
       iter[ self.class.id[:object] ] = object
     end
@@ -48,6 +51,20 @@ module ActiveWindow
             set_value(iter, column.id, column.data_value(object))
           end
           break
+        end
+      end
+    end
+
+    private
+    def update_iter_from_object(iter, object)
+      used_columns.each do |column|
+        iter[column.id] = column.data_value(object)
+      end
+    end
+    def update_iter_from_hash(iter, hash = {})
+      hash.symbolize_keys.each do |key,value|
+        if id = self.class.id[key]
+          iter[ id ] = value
         end
       end
     end
