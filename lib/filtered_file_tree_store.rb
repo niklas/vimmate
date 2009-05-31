dbb55c59f0d6267892390afb116056de3ad58420
Niklas Hofer <niklas+dev@lanpartei.de>
2009-05-31 02:48:54 +0200

    fuzzy slash filter

--- lib/filtered_file_tree_store.rb
+++ lib/filtered_file_tree_store.rb
@@ -1,11 +1,34 @@
 class FilteredFileTreeStore < ActiveWindow::FilteredActiveTreeStore
+  # Fuzzy search by String
+  # 'foo'     => matches la/lu/foo, f/lala/o/gaga/o
+  # 'foo/bar' => matches la/afoo/gnarz/barz, but not the above
   def iter_visible?(iter)
-    case iter[ self.class.column_id[:object] ]
+    case iter[OBJECT]
     when ListedDirectory; false
     when ListedFile
-      !(iter[self.class.column_id[:name]].index(filter_string)).nil?
+      iter[filter_column] =~ filter_regexp
     else
       false
     end
   end
+
+  def filter_string=(new_filter_string)
+    @filter_regexp = nil
+    @filter_column = nil
+    super
+  end
+
+
+  def filter_regexp
+    @filter_regexp ||= Regexp.new(
+      filter_string.split('/').map { |t| 
+        Regexp.escape(t).split(//).join('.*') 
+      }.join('.*/.*')
+    )
+  end
+
+  def filter_column
+    @filter_column ||= filter_string =~ %r~/~ ? FULL_PATH : NAME
+  end
+
 end
