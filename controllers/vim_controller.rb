4a2e508aba93e0e4caf1f5a704ec86a20d0e272d
Niklas Hofer <niklas+dev@lanpartei.de>
2009-05-31 14:03:15 +0200

    vim is used correctly again

--- controllers/vim_controller.rb
+++ controllers/vim_controller.rb
@@ -1,14 +1,14 @@
 class VimController < ActiveWindow::Controller
   attr_accessor :vim
 
-  def post_setup
+  def setup
     @vim = VimMate::VimWidget.new
     main_pane.pack2(vim.window, true, false)
   end
 
   def open_selected_file(args={})
     if iter = file_tree_view.selection.selected
-      mode = Config[:files_default_open_in_tabs] ? :tab : :open
+      mode = VimMate::Config[:files_default_open_in_tabs] ? :tab : :open
       vim.open iter[ FileTreeStore.full_path_column ], mode
     end
   end
