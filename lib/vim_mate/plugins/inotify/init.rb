f8a1c6eff203a9ae1bdd3a6deb5db6150940068f
Niklas Hofer <niklas+dev@lanpartei.de>
2011-01-04 02:45:35 +0100

    disable iNotify if no syscall() available

--- lib/vim_mate/plugins/inotify/init.rb
+++ lib/vim_mate/plugins/inotify/init.rb
@@ -1,4 +1,13 @@
 require 'lib/INotify'
 require 'lib/directory'
-ListedDirectory.class_eval { include VimMate::Plugin::INotifyDirectory }
+
+# FIXME current inotify implementation uses syscall
+#       syscall is not implemented in Ruby 1.9 on 64bit systems, see:
+#       * https://github.com/ruby/ruby/blob/ruby_1_9_2/io.c#L7529
+#       * https://github.com/ruby/ruby/commit/5a809d60bd3ea54474e34f897ae60b1ae78bab73#L0R6981
+#       * http://blade.nagaokaut.ac.jp/cgi-bin/scat.rb/ruby/ruby-core/8722
+
+if Kernel.respond_to?(:syscall)
+  ListedDirectory.class_eval { include VimMate::Plugin::INotifyDirectory }
+end
 
